8613fad4ac73fd89846c5b61caab058bc710d9b0
Aiman <aiman.waseem@evantagesoft.com>
2026-07-09 12:40:58 +0500

    up

---
+++ backend/src/server.js
@@ -0,0 +1,33 @@
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import pdfRoutes from "./routes/pdfRoutes.js";
+// import { allRoutes } from "./route.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// // app.use("/api/pdf", pdfRoutes);
+// allRoutes(app);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
